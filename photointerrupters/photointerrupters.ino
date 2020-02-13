481bb7d4b18f0ef3a98837cd3cac8018004f597a
Ariela L Milstein <amilste16@ccs.local>
2020-02-13 15:04:28 -0500

    all

--- photointerrupters/photointerrupters.ino
+++ photointerrupters/photointerrupters.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
-
+const int GatePin2 = 3; 
+const int GatePin3 = 2;
+const int LEDPin = 9;
+int counter = 0;
+int Blocked;
+ 
+ 
+void setup()
+{
+Serial.begin(9600);
+pinMode(LEDPin, OUTPUT);
+attachInterrupt(GatePin2, LEDon, RISING);
+attachInterrupt(GatePin3, LEDoff, FALLING); 
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+ 
+void loop(){
+ 
 }
+ 
+ 
+ void LEDon()
+{
+  digitalWrite(LEDPin, HIGH); 
+  counter ++; 
+  Serial.println("");
+  Serial.print("LEDon Interruptions:");
+  Serial.print(counter);
+ 
+}
+void LEDoff()
+{
+  digitalWrite(LEDPin, LOW); }
